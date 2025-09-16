01335b6062c1cbeb7843c3e6d7e7fe8a00225858
moth1995 <marcos-flores@live.com.ar>
2025-09-16 16:42:07 -0300

    Add unit tests for the basic helpers

---
+++ PES5_WE9_LE_CameraTool.Tests/ClippingAndConfigTests.cs
@@ -0,0 +1,109 @@
+﻿using NUnit.Framework;
+using PES5_WE9_LE_CameraTool;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Json;
+using System.Text;
+
+namespace CameraTool.Tests
+{
+    [TestFixture]
+    public class ClippingAndConfigTests
+    {
+        [Test]
+        public void Clipping_ToString_IncludesOffsetAndByteArrays()
+        {
+            var c = new Clipping();
+            c.offset = 0x12345678;
+            c.orgValue = new byte[] { 0x01, 0x02, 0x03, 0x04 };
+            c.newValue = new byte[] { 0xAA, 0xBB, 0xCC, 0xDD };
+
+            string s = c.ToString();
+
+            // Be tolerant to exact wording/format but check key parts are present.
+            StringAssert.Contains("305419896", s); // 0x12345678 decimal
+            StringAssert.Contains("01-02-03-04", s);
+            StringAssert.Contains("AA-BB-CC-DD", s);
+        }
+
+        [Test]
+        public void Configuration_Json_MinimalRoundtrip_Works()
+        {
+            string json = "{"
+                + "\"executableSize\":123456,"
+                + "\"cameraZoomOffset\":4096,"
+                + "\"cameraZoomShift\":0,"
+                + "\"cameraZoomOutOffset1\":8192,"
+                + "\"cameraZoomOutShift1\":0,"
+                + "\"cameraZoomOutOffset2\":12288,"
+                + "\"cameraZoomOutShift2\":0,"
+                + "\"stadRoofOffset1\":20480,"
+                + "\"stadRoofShift1\":0,"
+                + "\"stadRoofOffset2\":24576,"
+                + "\"stadRoofShift2\":0,"
+                + "\"clippingList\":[]"
+                + "}";
+
+            Configuration cfg;
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(json)))
+            {
+                var ser = new DataContractJsonSerializer(typeof(Configuration));
+                cfg = (Configuration)ser.ReadObject(ms);
+            }
+
+            Assert.IsTrue(string.IsNullOrEmpty(cfg.name));
+            Assert.AreEqual(123456u, cfg.executableSize);
+            Assert.AreEqual(4096u, cfg.cameraZoomOffset);
+            Assert.AreEqual(0, cfg.cameraZoomShift);
+        }
+
+        [Test]
+        public void Configuration_FromFilename_AssignsNameFromJsonFileName()
+        {
+            // Arrange: write a temp JSON *without* "name"; the app uses the file name as cfg.name
+            string json = "{"
+                + "\"executableSize\":1,"
+                + "\"cameraZoomOffset\":2,"
+                + "\"cameraZoomShift\":3,"
+                + "\"cameraZoomOutOffset1\":4,"
+                + "\"cameraZoomOutShift1\":5,"
+                + "\"cameraZoomOutOffset2\":6,"
+                + "\"cameraZoomOutShift2\":7,"
+                + "\"stadRoofOffset1\":8,"
+                + "\"stadRoofShift1\":9,"
+                + "\"stadRoofOffset2\":10,"
+                + "\"stadRoofShift2\":11,"
+                + "\"clippingList\":[]"
+                + "}";
+
+            string expectedName = "PES5_PS2_SLES_535_44";
+            string tmpDir = Path.Combine(Path.GetTempPath(), "cfg-tests-" + Guid.NewGuid().ToString("N"));
+            Directory.CreateDirectory(tmpDir);
+            string path = Path.Combine(tmpDir, expectedName + ".json");
+            File.WriteAllText(path, json, new UTF8Encoding(encoderShouldEmitUTF8Identifier: false));
+
+            try
+            {
+                // Act: load like the app (deserialize, then set name from file name)
+                Configuration cfg;
+                using (var fs = File.OpenRead(path))
+                {
+                    var ser = new DataContractJsonSerializer(typeof(Configuration));
+                    cfg = (Configuration)ser.ReadObject(fs);
+                }
+
+                // Simulate app behavior: name := file name (without extension)
+                cfg.name = Path.GetFileNameWithoutExtension(path);
+
+                // Assert
+                Assert.AreEqual(expectedName, cfg.name);
+            }
+            finally
+            {
+                // Cleanup
+                try { File.Delete(path); } catch { /* ignore */ }
+                try { Directory.Delete(tmpDir, recursive: true); } catch { /* ignore */ }
+            }
+        }
+    }
+}
---
+++ PES5_WE9_LE_CameraTool.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PES5_WE9_LE_CameraTool.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PES5_WE9_LE_CameraTool.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("610fb2e5-f229-4c18-824d-7bcdf4c4d9c6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
